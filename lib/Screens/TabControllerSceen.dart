28a3d719cfb09653e3b5935f4e1a8ab6630ce068
Rasidori <jmhsed2000@gmail.com>
2022-12-16 23:24:07 +0200

    Added Navbar and Appbar

---
+++ lib/Screens/TabControllerSceen.dart
@@ -0,0 +1,65 @@
+import 'package:autobid/Custom/CustomAppBar.dart';
+import 'package:flutter/material.dart';
+
+import 'ChatsScreen.dart';
+import 'ExploreScreen.dart';
+import 'FavoritesScreen.dart';
+import 'ListingsScreen.dart';
+
+class TabControllerScreen extends StatefulWidget {
+  const TabControllerScreen({super.key});
+
+  @override
+  State<TabControllerScreen> createState() => _TabControllerScreenState();
+}
+
+class _TabControllerScreenState extends State<TabControllerScreen> {
+  final List<Widget> pages = [
+    const ExploreScreen(),
+    const ChatsScreen(),
+    const ListingsScreen(),
+    const FavoritesScreen()
+  ];
+
+  final List<String> labels = [
+    'Explore',
+    'Chats',
+    'Listings',
+    'Favorites'
+  ];
+
+  int pageIndex = 0;
+
+  void pageSelect(int index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(title: labels[pageIndex]),
+      body: pages[pageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        elevation: 10,
+        selectedItemColor: Colors.pink,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded), label: labels[0]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble_rounded), label: labels[1]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.list_alt_rounded), label: labels[2]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star_rounded), label: labels[3])
+        ],
+        currentIndex: pageIndex,
+        onTap: pageSelect,
+      ),
+    );
+  }
+}
